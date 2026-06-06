dc54fa37c6333288a3434b58bbc31638fa217586
Rauf Qaracayev <qaracayevrauf9@gmail.com>
2026-06-06 11:09:14 +0400

    Add WebRTC camera/mic permissions + runtime request for voice/video calls

--- android/app/src/main/java/az/evden/mobile/MainActivity.java
+++ android/app/src/main/java/az/evden/mobile/MainActivity.java
@@ -1,9 +1,38 @@
 package az.evden.mobile;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.getcapacitor.BridgeActivity;
 import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
 public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // WebRTC səsli/görüntülü zəng üçün mikrofon + kamera icazəsi
+        String[] perms = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+        };
+        boolean need = false;
+        for (String p : perms) {
+            if (ContextCompat.checkSelfPermission(this, p) != PackageManager.PERMISSION_GRANTED) {
+                need = true;
+                break;
+            }
+        }
+        if (need) {
+            ActivityCompat.requestPermissions(this, perms, 1001);
+        }
+    }
 
     @Override
     public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
